21a765a5bd6dbce0e91c70e2b88f778d3e68fda9
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-25 11:23:53 +0530

    databinding

--- src/app/view-books/view-books.component.ts
+++ src/app/view-books/view-books.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-books.component.css']
 })
 export class ViewBooksComponent {
-
+data:any=[
+  {
+  "name":"Educated",
+  "image":"https://s26162.pcdn.co/wp-content/uploads/2018/12/educated.jpg",
+  "author":"Alex"
+}
+,
+{
+  "name":"The Book",
+  "image":"https://s26162.pcdn.co/wp-content/uploads/2018/12/184224_1326895.794x1200_q95_crop-smart_upscale.jpg",
+  "author":"Tony"
+},
+{
+  "name":"The Hypocrite World",
+  "image":"https://pub-static.fotor.com/assets/projects/pages/dddda0b59fb9433eb53e7174981c8b67/blue-minimal-novel-cover-6e355184dc3545c6bec6a9f618f83e0d.jpg",
+  "author":"Mark"
+},
+{
+  "name":"Memory",
+  "image":"https://static-cse.canva.com/blob/921487/ColorfulIllustrationYoungAdultBookCover.jpg",
+  "author":"Willison"
+},
+{
+  "name":"The past is Rising",
+  "image":"https://d1csarkz8obe9u.cloudfront.net/posterpreviews/contemporary-fiction-night-time-book-cover-design-template-1be47835c3058eb42211574e0c4ed8bf_screen.jpg?ts=1637012564",
+  "author":"Anill"
+},
+{
+  "name":"Front Page Fatality",
+  "image":"https://m.media-amazon.com/images/I/51FiPTWjtPL.jpg",
+  "author":"Smith"
+},
+{
+  "name":"Front Page Fatality",
+  "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLmdo3sdxH_K4XjtGA1n0j86-AEOfLOuq4KQ&usqp=CAU",
+  "author":"Alex"
+},
+{
+  "name":"Design Guide",
+  "image":"https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg",
+  "author":"David"
+},
+{
+  "name":"Harry Potter",
+  "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYdOaRZFJ1X87dK5Fs4tBqlGsCzLeTYD3Phw&usqp=CAU",
+  "author":"J K Rowling"
+}
+,
+{
+  "name":"Harry Potter",
+  "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLxPLIDtxDyPNZsGbZf7OOCXZa4FIQqwLGqFCoQMJDYymfNgHJnQAvXl72gmRJ5TTiANM&usqp=CAU",
+  "author":"J K Rowling"
+}]
 }
